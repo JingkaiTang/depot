b4b3cef99eec97cc1a89a67100ab73dcdb75abb0
Jingkai Tang <jingkaitang@gmail.com>
2015-07-10 10:49:05 +0800

    Sending e-Mail and Integration Testing After Chapter 13

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,27 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   address: "smtp.163.com",
+#   port: 25,
+#   authentication: :login,
+#   user_name: "liguoserver",
+#   password: "liguoshe",
+#   enable_startttls_auto: true
+# }
+
+# Bad mail settings
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.163.com",
+    port: 25,
+    authentication: :login,
+    user_name: "liguoserver",
+    password: "liguoshe",
+    enable_starttls_auto: true
+  }
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true 
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -39,3 +39,16 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+
+# Depot::Application.configure do
+#   config.action_mailer.delivery_method = :smtp
+
+#   config.action_mailer.smtp_settings = {
+#     address: "smtp.163.com",
+#     port: 25,
+#     authentication: :login,
+#     user_name: "liguoserver",
+#     password: "liguoshe",
+#     enable_starttls_auto: true
+#   }
+# end
